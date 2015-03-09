a8dca7a36460d89041e0ec23f7f3e9525951391d
Julian <julian.alonso.dev@gmail.com>
2015-03-09 12:40:08 +0100

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Julian Alonso on 9/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Julian Alonso on 9/3/15.
+//  Copyright (c) 2015 Julian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
